eff9564c94dcdd8dfa1569d43fe79dddddfbbd5d
khalid_shaik <khaliduddin@gmail.com>
2025-05-08 16:21:39 +0400

    compile data updated

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,14 +2,21 @@ require("@nomicfoundation/hardhat-toolbox");
 
 require("dotenv").config;
 
+BSC_RPC='https://bsc-testnet-rpc.publicnode.com'
+BSC_PRIV_KEY=""
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.28",
   defaultNetwork: "bsc",
   networks: {
+    // "bsc_1": {
+    //   url: process.env.BSC_RPC,
+    //   accounts: [ process.env.BSC_PRIV_KEY ],
+    // },
     "bsc": {
-      url: process.env.BSC_RPC,
-      accounts: [ process.env.BSC_PRIV_KEY ],
+      url: BSC_RPC,
+      accounts: [ BSC_PRIV_KEY ],
     },
   }
 };
